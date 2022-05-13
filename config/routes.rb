411698e6f46285aa4fb192871ef9236918b6635a
James Campbell <jcampbell92@icloud.com>
2022-05-13 12:05:39 -0400

    added heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/', to: 'lists#index'
   get 'lists', to: 'lists#index'
   get 'lists/new', to: 'lists#new', as: :new_list
   get 'lists/:id', to: 'lists#show', as: :list
